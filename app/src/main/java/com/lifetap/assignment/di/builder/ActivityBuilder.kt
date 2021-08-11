263a673fa3c5b6638e466581414bf5eaed5c25a3
Vidit <vidit.gour@mobileprogrammingllc.com>
2021-08-12 01:57:12 +0530

    Adding final styles and dummy image

--- app/src/main/java/com/lifetap/assignment/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/lifetap/assignment/di/builder/ActivityBuilder.kt
@@ -1,6 +1,6 @@
 package com.lifetap.assignment.di.builder
 
-import com.lifetap.assignment.ui.MainActivity
+import com.lifetap.assignment.ui.bubble.MainActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
--- app/src/main/java/com/lifetap/assignment/ui/bubble/MainActivity.kt
+++ app/src/main/java/com/lifetap/assignment/ui/bubble/MainActivity.kt
@@ -1,24 +1,37 @@
-package com.lifetap.assignment.ui
+package com.lifetap.assignment.ui.bubble
 
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
 import com.lifetap.assignment.R
 import com.lifetap.assignment.base.BaseActivity
 import com.lifetap.assignment.databinding.ActivityMainBinding
+import com.lifetap.assignment.ui.bubble.item.ItemViewModelDelegate
 
-class MainActivity : BaseActivity() {
+import javax.inject.Inject
 
+
+class MainActivity : BaseActivity(), ItemViewModelDelegate {
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        viewModel.delegate = this
+        viewModel.setValues()
 
     }
+
+//Can be used to show some activity after click
+    override fun onImageClick() {
+        // do nothing for now
+    }
+
+    override val ctx: Context
+        get() = this@MainActivity
 
 
 }
